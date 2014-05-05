2f943b0da6e59aef12edfdd1cb2f6382d2a94f77
Manish Chavan <manishc1@umbc.edu>
2014-05-05 03:19:31 -0400

    Added output presentation logic.

---
+++ output.py
@@ -0,0 +1,37 @@
+"""
+Stores the result of the instruction.
+"""
+
+class Output():
+    """
+    Class to present the resulting output.
+    """
+
+    def __init__(self, instruction, cycle = 0):
+        """
+        Initialize the output.
+        """
+        self.instruction = instruction
+        self.cycles = {'IF': cycle, 'ID': 0, 'EX': 0, 'WB': 0}
+        self.hazards = {'RAW': False, 'WAR': False, 'WAW': False, 'Struct': False}
+
+
+    def __str__(self):
+        """
+        Convert to string representation.
+        """
+        string = str(self.instruction) + '\t'
+
+        for stage in ['ID', 'IF', 'EX', 'WB']:
+            if (self.cycles[stage] != 0):
+                string += str(self.cycles[stage]) + '\t'
+            else:
+                string += '\t'
+
+        for hazard in ['RAW', 'WAR', 'WAW', 'Struct']:
+            if (self.hazards[hazard]):
+                string += 'Y\t'
+            else:
+                string += 'N\t'
+
+        return string
